844c8652aa67c4a8926895849e9adfe6f2a7ac69
yoan8306 <yoan8306@wanadoo.fr>
2021-10-02 23:10:29 +0200

    newModel integrate calculation priority

--- Projet_CountOnMe-master/CountOnMe/Controller/ViewController.swift
+++ Projet_CountOnMe-master/CountOnMe/Controller/ViewController.swift
@@ -9,15 +9,15 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
+
     var calculator = SimpleCalc()
     var expressionHaveResult: Bool {
         return textView.text.firstIndex(of: "=") != nil
     }
-    
+
     @IBOutlet weak var textView: UITextView!
     @IBOutlet var numberButtons: [UIButton]!
-    
+
     // View actions
     @IBAction func tappedNumberButton(_ sender: UIButton) {
         if expressionHaveResult {
@@ -26,53 +26,54 @@ class ViewController: UIViewController {
         guard let numberText = sender.title(for: .normal) else {
             return
         }
-        calculator.addNumber(element: numberText)
-//
-        
+        calculator.addNumber(number: numberText)
         textView.text.append(numberText)
     }
-    
+
     @IBAction func tappedOperatorButton(_ sender: UIButton) {
-        
         guard let textOperator = sender.title(for: .normal) else {
             return
         }
-        if calculator.addOperator(addOperation: textOperator) {
 
-        switch sender.titleLabel?.text {
-        case "+":
-            textView.text.append(" + ")
-        case "÷":
-            textView.text.append(" ÷ ")
-        case "x":
-            textView.text.append(" x ")
-        case "-":
-            textView.text.append(" - ")
-        default:
-            presentAlert_Alert(alertTitle: "Fault", alertMessage: "Erreur inconnu", buttonTitle: "Ok", alertStyle: .cancel)
-        }
+        if calculator.addOperator(newOperator: textOperator) {
+            switch sender.titleLabel?.text {
+            case "+":
+                textView.text.append(" + ")
+            case "÷":
+                textView.text.append(" ÷ ")
+            case "x":
+                textView.text.append(" x ")
+            case "-":
+                textView.text.append(" - ")
+            default:
+                presentAlert_Alert(alertTitle: "Fault",
+                                   alertMessage: "Erreur inconnu",
+                                   buttonTitle: "Ok", alertStyle: .cancel)
+            }
+
         } else {
-            presentAlert_Alert(alertTitle: "Erreur", alertMessage: "Veuiller inscrire un nombre en premier", buttonTitle: "Ok", alertStyle: .cancel)
+            presentAlert_Alert(alertTitle: "Erreur",
+                               alertMessage: "Veuillez inscrire un nombre avant",
+                               buttonTitle: "Ok", alertStyle: .cancel)
         }
-            
     }
-    
+
     @IBAction func tappedEqualButton(_ sender: UIButton) {
         if let calculTotal = calculator.equal() {
-            textView.text.append(" = " + String(calculTotal))
-            calculator.resetCalcul()
+            textView.text.append(" = " + calculTotal)
         } else {
-            presentAlert_Alert(alertTitle: "Erreur", alertMessage: "Veuillez entrer une expression correcte", buttonTitle: "Ok", alertStyle: .cancel)
+            presentAlert_Alert(alertTitle: "Erreur",
+                               alertMessage: "Veuillez entrer une expression correcte",
+                               buttonTitle: "Ok", alertStyle: .cancel)
         }
     }
-    
-    private func presentAlert_Alert (alertTitle title: String, alertMessage message: String,buttonTitle titleButton: String, alertStyle style: UIAlertAction.Style ) {
+
+    private func presentAlert_Alert (alertTitle title: String,
+                                     alertMessage message: String,
+                                     buttonTitle titleButton: String, alertStyle style: UIAlertAction.Style ) {
         let alertVC = UIAlertController(title: title, message: message, preferredStyle: .alert)
         let action = UIAlertAction(title: titleButton, style: style, handler: nil)
         alertVC.addAction(action)
         present(alertVC, animated: true, completion: nil)
     }
-    
-    
 }
-
--- Projet_CountOnMe-master/CountOnMe/Model/SimpleCalc.swift
+++ Projet_CountOnMe-master/CountOnMe/Model/SimpleCalc.swift
@@ -9,87 +9,121 @@
 import Foundation
 
 class SimpleCalc {
-    
     var arrayElement: [String] = []
-    private let arrayOperator = ["+", "-", "÷", "x"]
-    private var firstNumber = ""
-    private var secondNumber = ""
-    private var operation = ""
-    private var result = 0
-    
-    public func addNumber(element: String) {
-        arrayElement.append(element)
-        if operation.isEmpty {
-            firstNumber = firstNumber + element
+    let listOperator = ["+", "-", "x", "÷"]
+
+    func addNumber(number: String) {
+        var stringNumber = ""
+        if let lastElement = arrayElement.last {
+            if listOperator.contains(lastElement) {
+                arrayElement.append(number)
+            } else {
+                stringNumber  = lastElement + number
+                arrayElement.removeLast()
+                arrayElement.append(stringNumber)
+            }
         } else {
-            secondNumber = secondNumber + element
+            arrayElement.append(number)
         }
     }
-    
-    public func addOperator(addOperation: String) -> Bool {
-        guard expressionIsCorrect(element: addOperation) else {
+
+    func addOperator(newOperator: String) -> Bool {
+        if canAddOperator() {
+            arrayElement.append(newOperator)
+            return true
+        }
+        return false
+    }
+
+    func equal() -> String? {
+        var operation = ""
+        var result = 0
+        var firstNumber = ""
+        var secondNumber = ""
+        var index = 0
+        var max = arrayElement.count
+
+        guard arrayElement.count >= 3 && !listOperator.contains(arrayElement.last ?? "+") else {
+            return nil
+        }
+
+        checkPriority()
+
+        while index < max {
+            firstNumber = arrayElement[0]
+            operation = arrayElement[1]
+            secondNumber = arrayElement[2]
+
+            if  let left = Int(firstNumber), let right = Int(secondNumber) {
+
+                switch operation {
+                case "+":
+                    result = left + right
+                case "-":
+                    result = left - right
+                case "÷":
+                    result = left / right
+                case "x":
+                    result = left * right
+                default:
+                    print("error")
+                }
+
+                arrayElement = Array(arrayElement.dropFirst(3))
+                arrayElement.insert("\(result)", at: 0)
+            }
+            max = arrayElement.count
+            index += 1
+        }
+        arrayElement = []
+        return String(result)
+    }
+
+    // MARK: - private func
+
+    private  func canAddOperator() -> Bool {
+        guard let lastElement = arrayElement.last else {
             return false
         }
-        if operation.isEmpty {
-            arrayElement.append(addOperation)
-            operation = addOperation
-        } else if expressionIsCorrect(element: addOperation) {
-            arrayElement.append(addOperation)
-            if let calculation = calcul() {
-                firstNumber = String(calculation)
-            }
-            secondNumber = ""
-            operation = addOperation
-        } else {
+
+        if listOperator.contains(lastElement) {
             return false
         }
         return true
     }
-    
-    public func equal() -> Int? {
-        guard let verify = arrayElement.last, expressionIsCorrect(element: verify) else {
-            return nil
+
+    private  func checkPriority() {
+        if checkOperation() {
+            calculationPriority()
         }
-        if calcul() != nil {
-            return calcul()
+    }
+
+    private  func checkOperation() -> Bool {
+        if arrayElement.contains("-") ||
+            arrayElement.contains("+") ||
+            arrayElement.contains("÷") && arrayElement.contains("x") {
+            return true
         }
-        return nil
+        return false
     }
-    
-    public func resetCalcul() {
-        arrayElement = []
-        firstNumber = ""
-        secondNumber = ""
-        operation = ""
-        result = 0
-    }
-    
-    private  func expressionIsCorrect(element: String) -> Bool {
-        guard let verify = arrayElement.last else {
-            return false
-        }
-        return !arrayOperator.contains(verify)
-    }
-    
-    private func calcul() -> Int? {
-        if  let left = Int(firstNumber), let right = Int(secondNumber) {
-            switch operation {
-            case "+":
-                result = left + right
-            case "-":
-                result = left - right
-            case "÷":
-                result = left / right
-            case "x":
-                result = left * right
-            default:
-                print("error")
+
+    private func calculationPriority() {
+        var max = arrayElement.count - 1
+        var index = 0
+        var result = 0
+        var newElement = ""
+
+        while index < max {
+            if arrayElement[index] == "x" {
+                if let leftNumber = Int(arrayElement[index - 1]), let rightNumber = Int(arrayElement[index + 1]) {
+                    result = leftNumber * rightNumber
+                    newElement = String(result)
+                    arrayElement.removeSubrange(index - 1 ... index + 1)
+                    arrayElement.insert(newElement, at: index - 1)
+                    max = arrayElement.count - 1
+                }
             }
-        } else {
-            print("Veuillez entrer un opérateur et un autre chiffre pour le calcul")
-            return nil
+            index += 1
         }
-        return result
     }
 }
-
